bcd0399e50f949e70c1d92357871a65b5a28f6d8
pristit <epicnesssauce@gmail.com>
2022-03-15 16:33:49 +0200

    WIP SampleServiceImpl createSample

--- src/main/java/CPR/Service/SampleServiceImpl.java
+++ src/main/java/CPR/Service/SampleServiceImpl.java
@@ -1,21 +1,49 @@
 package CPR.Service;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Service;
 
 import CPR.Boundary.SampleBoundary;
+import CPR.Data.SampleConverter;
 import CPR.Data.SampleEntity;
 
 @Service
 public class SampleServiceImpl implements SampleService {
 
 	private Map<String, SampleEntity> storage;
+	private SampleConverter converter;
+	private AtomicLong counter;
+	
+	@PostConstruct
+	public void init()
+	{
+		// initialize thread safe storage
+		this.storage = Collections.synchronizedMap(new HashMap<>());
+		
+		// initialize counter
+		this.counter = new AtomicLong(1L);		
+	}
 	
 	@Override
 	public Object createSample(SampleBoundary sample) {
-		// TODO Auto-generated method stub
-		return null;
+		SampleEntity sampleEntity = converter.convertToEntity(sample);
+		sampleEntity.setSample_Id(String.valueOf(counter.getAndIncrement()));
+		
+		if (sampleEntity.getSample_Id() != null && sampleEntity.getSession_Id() != null)
+		{
+			storage.put(sampleEntity.getSample_Id(), sampleEntity);
+			return sample;
+		}
+		else {
+			System.out.println("\nERROR!!!!!!! createSample in SampleServiceImpl\n");
+			return converter.convertToBoundary(new SampleEntity("NULL", "NULL", new String[] {"NULL"}));
+		}
 	}
 
 	@Override
