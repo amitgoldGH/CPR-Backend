d69519adb613dbe1338a253fbcd901947352462e
pristit <epicnesssauce@gmail.com>
2022-03-15 17:37:29 +0200

    Added Autowire for converter, and init for mem storage and counter

--- src/main/java/CPR/Service/SessionServiceImpl.java
+++ src/main/java/CPR/Service/SessionServiceImpl.java
@@ -1,17 +1,41 @@
 package CPR.Service;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import CPR.Boundary.SessionBoundary;
+import CPR.Data.SessionConverter;
 import CPR.Data.SessionEntity;
 
 @Service
 public class SessionServiceImpl implements SessionService{
 
 	private Map<String, SessionEntity> storage;
-
+	private SessionConverter converter;
+	private AtomicLong counter;
+	
+	@Autowired
+	public SessionServiceImpl(SessionConverter converter) {
+		this.converter = converter;
+	}
+	
+	@PostConstruct
+	public void init()
+	{
+		// initialize thread safe storage
+		this.storage = Collections.synchronizedMap(new HashMap<>());
+		
+		// initialize counter
+		this.counter = new AtomicLong(1L);		
+	}
 	@Override
 	public Object createSession(SessionBoundary session) {
 		// TODO Auto-generated method stub
@@ -19,13 +43,13 @@ public class SessionServiceImpl implements SessionService{
 	}
 
 	@Override
-	public Object[] getAllSessions() {
+	public List<Object> getAllSessions() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Object[] getAllSessionsByUser(String username) {
+	public List<Object> getAllSessionsByUser(String username) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/main/java/CPR/Service/UserServiceImpl.java
+++ src/main/java/CPR/Service/UserServiceImpl.java
@@ -1,18 +1,42 @@
 package CPR.Service;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import CPR.Boundary.NewUserBoundary;
 import CPR.Boundary.UserBoundary;
+import CPR.Data.UserConverter;
 import CPR.Data.UserEntity;
 
 @Service
 public class UserServiceImpl implements UserService{
 
 	private Map<String, UserEntity> storage;
-
+	private UserConverter converter;
+	private AtomicLong counter;
+	
+	@Autowired
+	public UserServiceImpl(UserConverter converter) {
+		this.converter = converter;
+	}
+	
+	@PostConstruct
+	public void init()
+	{
+		// initialize thread safe storage
+		this.storage = Collections.synchronizedMap(new HashMap<>());
+		
+		// initialize counter
+		this.counter = new AtomicLong(1L);		
+	}
 	
 	@Override
 	public Object createUser(NewUserBoundary new_User_Boundary) {
@@ -33,7 +57,7 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
-	public Object[] getAllUsers() {
+	public List<Object> getAllUsers() {
 		// TODO Auto-generated method stub
 		return null;
 	}
