e02fb4aef094ea61203f456ece94b8d9e05c2da7
Amit <Amit@10.100.102.18>
2022-03-15 13:45:41 +0200

    Created controller mapping stubs for Samples
    For Sample controller:
    TYPE_POST Create new Sample
    TYPE_GET Get all samples of specific session id
    TYPE_PUT Update specific sample

--- src/main/java/CPR/Controller/SampleController.java
+++ src/main/java/CPR/Controller/SampleController.java
@@ -1,6 +1,7 @@
 package CPR.Controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,16 +13,43 @@ import CPR.Boundary.SampleBoundary;
 public class SampleController {
 	
 	
-	//POST request, path="/iob/activities"
-	//Accept: Activity Boundary with null activityId
-	//Return: Any JSON Object
+	//POST request, path="/api/samples"
+	//Accept: Sample Boundary
+	//Return: SampleBoundary
 	@RequestMapping(
-			path = "/cpr/samples",
+			path = "/api/samples",
 			method = RequestMethod.POST,
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Object invokeActivity(@RequestBody SampleBoundary sample) {
-		System.out.println(sample.toString());
-		return sample;
+	public Object createSample(@RequestBody SampleBoundary sample) {
+		System.out.println("/api/samples CREATE SAMPLE TYPE_POST called " + sample.toString());
+		return sample; // TODO: Return sample via service
+	}
+	
+	//GET request, path="/api/samples/session/{id}"
+	//Accept: None
+	//Return: SampleBoundary array
+	@RequestMapping(
+			path="/api/samples/session/{sessionId}",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+		public Object[] retrieveAllSessionSamples(
+				@PathVariable("sessionId") String session_Id){
+			System.out.println("/api/samples/session/" + session_Id + " RETRIEVE ALL SAMPLES OF GIVEN SESSION ID TYPE_GET called");
+			return new SampleBoundary[0]; // TODO: Return all samples with given session id
+			//return userService.login(userDomain, userEmail);
+		}
+	
+	//PUT request, path="/api/samples"
+	//Accept: Sample boundary with id
+	//Return: Nothing
+	@RequestMapping(
+			path="/api/samples",
+			method = RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public void updateSample(@RequestBody SampleBoundary sample) {
+		System.out.println("/api/samples UPDATE SAMPLE TYPE_PUT called " + sample.toString());
+		// TODO: Implement updating sample
 	}
 }
