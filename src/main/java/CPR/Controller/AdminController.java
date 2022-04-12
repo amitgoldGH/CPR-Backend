99dcc7bc3f335a092c67f32aafcb79a88bc474fc
pristit <epicnesssauce@gmail.com>
2022-04-12 17:49:55 +0300

    removed redundant import

--- src/main/java/CPR/Controller/AdminController.java
+++ src/main/java/CPR/Controller/AdminController.java
@@ -1,7 +1,5 @@
 package CPR.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/CPR/Controller/SampleController.java
+++ src/main/java/CPR/Controller/SampleController.java
@@ -1,6 +1,5 @@
 package CPR.Controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
